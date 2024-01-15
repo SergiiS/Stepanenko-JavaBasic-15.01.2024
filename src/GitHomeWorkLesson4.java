fb55e1b36002564891a3bbb089474878ad1cebda
sergiistepanenko <sergiystep@gmail.com>
2024-01-15 18:04:42 +0200

    Second commit

--- src/GitHomeWorkLesson4.java
+++ src/GitHomeWorkLesson4.java
@@ -1,5 +1,6 @@
 public class GitHomeWorkLesson4 {
     public static void main(String[] args) {
         System.out.println("Hello Java");
+        System.out.println("Hello from Sergii to Git!");
     }
 }
